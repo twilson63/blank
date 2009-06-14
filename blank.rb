beeadbee524cae073c071d3dca0b18e8dc5a0873
Thomas Wilson <twilson63@ubuntu.(none)>
2009-06-14 09:32:27 -0400

    added form helpers

--- blank.rb
+++ blank.rb
@@ -6,6 +6,7 @@ require 'activerecord'
 require 'crack'
 require 'rest_client'
 require 'cgi'
+require 'lib/helpers'
 
 dbconfig = YAML.load(File.read('config/database.yml'))
 ActiveRecord::Base.establish_connection dbconfig['production']
